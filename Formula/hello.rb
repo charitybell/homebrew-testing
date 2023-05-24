05c1632dd3cdecdc1d314a831b8f22bcb0da8c3a
charitybell <128868596+charitybell@users.noreply.github.com>
2023-05-24 03:25:22 +0000

    hello: add 2.12_16 bottle.

--- Formula/hello.rb
+++ Formula/hello.rb
@@ -6,6 +6,10 @@ class Hello < Formula
   sha256 "cf04af86dc085268c5f4470fbae49b18afbc221b78096aab842d934a76bad0ab"
   license "AGPL-3.0-only"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "c752d694cb6bbf34882c25b281bead93c20aa7614c6d6a0b9508f1ee36027380"
+  end
+
   depends_on "gcc" => :build
   depends_on "go" => :build
   depends_on "node" => :build
