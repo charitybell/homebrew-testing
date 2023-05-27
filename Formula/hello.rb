396b44be698b59c7240869affa77bf1e971d7bd1
charitybell <hello@charitybell.xyz>
2023-05-27 01:18:40 +0000

    test

--- Formula/hello.rb
+++ Formula/hello.rb
@@ -1,5 +1,5 @@
 class Hello < Formula
-  revision 80
+  revision 81
   desc "gnu hello"
   homepage "homepage"
   url "https://ftp.gnu.org/gnu/hello/hello-2.12.tar.gz"
--- Formula/hello2.rb
+++ Formula/hello2.rb
@@ -1,5 +1,5 @@
 class Hello2 < Formula
-  revision 1
+  revision 2
   desc "gnu hello"
   homepage "homepage"
   url "https://ftp.gnu.org/gnu/hello/hello-2.12.tar.gz"
