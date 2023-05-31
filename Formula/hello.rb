41cfb28dab601eb2c65ab6a00a3c3a86d292b8b0
charitybell <hello@charitybell.xyz>
2023-05-31 07:20:11 +0000

    test

---
+++ Formula/hello.rb
@@ -0,0 +1,21 @@
+class Hello < Formula
+  desc "Program providing model for GNU coding standards and practices"
+  homepage "https://www.gnu.org/software/hello/"
+  url "https://ftp.gnu.org/gnu/hello/hello-2.12.1.tar.gz"
+  sha256 "8d99142afd92576f30b0cd7cb42a8dc6809998bc5d607d88761f512e26c7db20"
+  license "GPL-3.0-or-later"
+
+  conflicts_with "perkeep", because: "both install `hello` binaries"
+
+  def install
+    ENV.append "LDFLAGS", "-liconv" if OS.mac?
+
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+  test do
+    assert_equal "brew", shell_output("#{bin}/hello --greeting=brew").chomp
+  end
+end
