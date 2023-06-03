da127276056cc3cccd0f0228595290e33ed5ae0d
charitybell <hello@charitybell.xyz>
2023-06-03 20:46:28 +0000

    Add revision to extract-iso to force github rebuild

--- Formula/extract-xiso.rb
+++ Formula/extract-xiso.rb
@@ -4,6 +4,7 @@ class ExtractXiso < Formula
   url "https://github.com/XboxDev/extract-xiso/archive/refs/tags/build-202303040307.tar.gz"
   sha256 "6b40a0114fb9f79d9e75a4d6c80efce68e2cb0da3e93fe575ea2c7e30aa5ed57"
   license "BSD-4-Clause"
+  revision 1
   head "https://github.com/XboxDev/extract-xiso.git", branch: "master"
 
   depends_on "cmake" => :build
