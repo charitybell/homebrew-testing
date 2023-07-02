b6d098f5359980733a1692ef8f5a4508f63dc3c4
charitybell <hello@charitybell.xyz>
2023-07-02 08:42:43 +0000

    Bump cemu-wiiu to 2.0-42

--- Casks/cemu-wiiu.rb
+++ Casks/cemu-wiiu.rb
@@ -1,6 +1,6 @@
 cask "cemu-wiiu" do
-  version "2.0-39"
-  sha256 "353bfa8508b6362063d9d6fe898b558bb739efd20ae7f4a50853eb6ec2fdf89a"
+  version "2.0-42"
+  sha256 "d177771b189efdafe269a81f79f334027188a9e66a9e84fe6251aa731cf69249"
   homepage "https://github.com/cemu-project/Cemu"
   url "https://github.com/cemu-project/Cemu/releases/download/v#{version}/cemu-#{version}-macos-12-x64.dmg"
   name "Cemu"
